00db959def1226374054fc272a3da49ab4727918
Hp <yahye2092@gmail.com>
2023-04-28 01:09:23 -0700

    first commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(express.json())
 app.use('/api/user', userRouter)
 
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000
 app.listen(()=>{
 console.log(`server is running ${port}`)
 })
